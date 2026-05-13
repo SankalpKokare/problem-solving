ff02f018e6ce96b03d3d4c605856e6fe31b38fd6
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-05-14 01:06:44 +0530

    Add: unique-email-addresses - submission-2

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        res = set()
+
+        for e in emails:
+            local, domain = e.split("@")
+            local = local.replace(".", "")
+            local = local.split("+")[0]
+            new_email = local + domain
+            res.add(new_email)
+
+        return len(res)
