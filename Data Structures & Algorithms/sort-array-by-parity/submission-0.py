54618083cc2f915673142110e59f5fa4367c2e64
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-05-16 17:36:25 +0530

    Add: sort-array-by-parity - submission-0

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+
+        res = [0] * len(nums)
+
+        even = 0
+        odd = len(nums) - 1
+
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                res[even] = nums[i]
+                even += 1
+            else:
+                res[odd] = nums[i]
+                odd -= 1
+
+        return res
