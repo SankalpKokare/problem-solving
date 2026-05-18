41a1fee2f5092c5e62f93fb9af59db1dbce44df0
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-05-19 00:54:40 +0530

    Add: path-crossing - submission-2

---
+++ Data Structures & Algorithms/path-crossing/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        dirc = {"N": [0, 1], "S": [0, -1], "W": [-1, 0], "E": [1, 0]}
+
+        visit = set()
+        x, y = 0, 0
+        for p in path:
+            visit.add((x, y))
+            dx, dy = dirc[p]
+            x += dx
+            y += dy
+            if (x, y) in visit:
+                return True
+
+        return False
