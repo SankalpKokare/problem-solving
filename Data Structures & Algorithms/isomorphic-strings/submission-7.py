74b7c12fdd84f19bf1d908b2f262464a0b4c41cb
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-05-22 00:54:16 +0530

    Add: isomorphic-strings - submission-7

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        st = {}
+        ts = {}
+
+        for i in range(len(s)):
+            if s[i] in st and st[s[i]] != t[i]:
+                return False
+
+            if t[i] in ts and ts[t[i]] != s[i]:
+                return False
+
+            st[s[i]] = t[i]
+            ts[t[i]] = s[i]
+
+        return True
