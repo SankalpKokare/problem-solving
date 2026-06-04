98098d56ad20862abe2fbcfc2e93bc8e01a4de70
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-06-04 23:30:50 +0530

    Add: ransom-note - submission-5

---
+++ Data Structures & Algorithms/ransom-note/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        countR = Counter(ransomNote)
+        countM = Counter(magazine)
+
+        for k in countR:
+            if countR[k] > countM[k]:
+                return False
+
+        return True
