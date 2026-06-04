a25cc30389d6c73be23cb7acac9d5218e630c11d
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-06-04 22:11:43 +0530

    Add: number-of-senior-citizens - submission-9

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        res = 0
+
+        for d in details:
+            phn = d[0:10]
+            g = d[10:11]
+            age = d[11:13]
+
+            if int(age) > 60:
+                res += 1
+
+        return res
