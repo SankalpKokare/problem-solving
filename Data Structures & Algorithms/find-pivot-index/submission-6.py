9f4cce2d300ce96e0aeb41695f6a7f05bb109b50
Sankalp Kokare <sankalpkokare21@gmail.com>
2026-06-04 22:44:32 +0530

    Add: find-pivot-index - submission-6

---
+++ Data Structures & Algorithms/find-pivot-index/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        prefix = [0] * (len(nums) + 1)
+
+        for i in range(len(nums)):
+            prefix[i + 1] = prefix[i] + nums[i]
+
+        for i in range(len(nums)):
+            prefix_sum = prefix[i]
+            suffix_sum = prefix[len(nums)] - prefix[i + 1]
+
+            if prefix_sum == suffix_sum:
+                return i
+
+        return -1
